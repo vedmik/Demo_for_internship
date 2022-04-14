953dadc1c520a8ec6605bf55658f694144248775
Andrii Vedmid <medvik9@gmail.com>
2022-04-14 12:21:41 +0300

    v1.1 worked release

--- src/main/java/ua/coparts/demo/repo/CarRepository.java
+++ src/main/java/ua/coparts/demo/repo/CarRepository.java
@@ -1,11 +1,11 @@
 package ua.coparts.demo.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.coparts.demo.models.Car;
 import ua.coparts.demo.models.User;
 
 @Repository
-public interface CarRepository extends CrudRepository<Car, Long> {
+public interface CarRepository extends JpaRepository<Car, Long> {
     Iterable<Car> findByOwner(User owner);
 }
--- src/main/java/ua/coparts/demo/repo/UserRepository.java
+++ src/main/java/ua/coparts/demo/repo/UserRepository.java
@@ -1,8 +1,10 @@
 package ua.coparts.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ua.coparts.demo.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
